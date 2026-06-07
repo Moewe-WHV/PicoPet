d76276022ae3262f9207b8aad59ddaee4aa1acb0
Tim K <klein_tim@t-online.de>
2026-06-08 01:23:43 +0200

    add: Werte hinzugefuegt

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
-# Platzhaltergi
+"""Hier sind Zeiten und Werte definiert. Keine Magic Numbers.
+    Args: 
+        Threading: 720
+        Kritischer Wert: 75
+        Bedürfniss sinkt: 5
+        Kritische Runden: 5
+        Gesundheitsabzug: 20
+        """
+threading_ticker = 720.0
+kritischer_beduerfniss_wert = 75
+bedüfniss_sinkt_um_wert = 5
+kritische_beduerfniss_runden = 5
+gesundheits_abzug = 20
