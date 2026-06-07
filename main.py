07af867dc3a769745966f04b02c5cdf9fe3b88de
Tim K <klein_tim@t-online.de>
2026-06-08 00:36:17 +0200

    feat: main Basisklasse angelegt

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# Einstiegspunkt
+from PicoPet.dino import Dino 
+
+dino = Dino(0,0,0,0,100)
+
+def main():
+    """Startet das Spiel."""
+    #Gui()
+    pass
+    
+if __name__ == "__main__":
+    print(dino.gesundheit_punkte_wert)
+
+    main()
