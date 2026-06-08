0c8c4c55f7f2e74ab6b8cb77ef25f144d77e475a
Tim K <klein_tim@t-online.de>
2026-06-08 12:09:29 +0200

    fix: Config import und Variablen angepasst.

--- PicoPet/base.py
+++ PicoPet/base.py
@@ -1,4 +1,5 @@
 import threading 
+from config import Werte
 
 class Beduerfnisse: 
     """Verwaltet die Bedürfnisse eines Tamagotchi-Tieres (Hunger, Dreck, Müdigkeit, Freude).
@@ -37,69 +38,64 @@ class Beduerfnisse:
             }
         }
 
-        timer = threading.Timer(720.0, self.beduerfnisse_steigen)
+        timer = threading.Timer(Werte.threading_ticker, self.beduerfnisse_steigen)
         timer.start()
         
     def beduerfnisse_steigen(self):
-        """Erhöht alle Bedürfniswerte um 5 und startet den Timer neu."""
+        """Erhöht alle Bedürfniswerte beinhaltet Gesundheitlogik und den Timer."""
         
         for etikett in self.__beduerfnisse:
-            self.__beduerfnisse[etikett]["wert"] += 5
+            self.__beduerfnisse[etikett]["wert"] += Werte.bedüfniss_sinkt_um_wert
             
-            if self.__beduerfnisse[etikett]["wert"] >= 80:
+            if self.__beduerfnisse[etikett]["wert"] >= Werte.kritischer_beduerfniss_wert:
                 self.__beduerfnisse[etikett]["runden_ueber_80"] += 1
                 print(self.__beduerfnisse[etikett]["nachricht"])   
             
-            if self.__beduerfnisse[etikett]["runden_ueber_80"] >= 4: 
-                self.__gesundheit -= 20
-            elif self.__beduerfnisse[etikett]["wert"]  < 80:
+            if self.__beduerfnisse[etikett]["runden_ueber_80"] >= Werte.kritische_beduerfniss_runden: 
+                self.__gesundheit -= Werte.gesundheits_abzug
+            elif self.__beduerfnisse[etikett]["wert"]  < Werte.kritischer_beduerfniss_wert:
                 self.__beduerfnisse[etikett]["runden_ueber_80"] = 0  
         
         if self.__gesundheit > 0:
-            timer = threading.Timer(720.0, self.beduerfnisse_steigen)
+            timer = threading.Timer(Werte.threading_ticker, self.beduerfnisse_steigen)
             timer.start()          
-        
+        elif self.__gesundheit < 20:
+            print("Ich werde Krank") 
+        if self.__gesundheit <= 0:
+            print("Dein Pet ist gestorben!!!!")
+            
     @property # Hunger
     def hunger_punkte_wert(self):
         """Gibt den aktuellen Hungerwert zurück."""
         return self.__beduerfnisse["hunger"]["wert"]
-    
     @hunger_punkte_wert.setter
     def hunger_punkte_wert(self, wert): 
-        self.__beduerfnisse["hunger"]["wert"] = wert
-        
+        self.__beduerfnisse["hunger"]["wert"] = wert     
     @property # Müdigkeit
     def muedigkeit_punkte_wert(self):
         """Gibt den aktuellen Müdigkeitswert zurück."""
-        return self.__beduerfnisse["muedigkeit"]["wert"]
-    
+        return self.__beduerfnisse["muedigkeit"]["wert"]  
     @muedigkeit_punkte_wert.setter
     def muedigkeit_punkte_wert(self, wert): 
-        self.__beduerfnisse["muedigkeit"]["wert"] = wert 
-        
+        self.__beduerfnisse["muedigkeit"]["wert"] = wert   
     @property # Langeweile
     def langeweile_punkte_wert(self):
         """Gibt den aktuellen Langeweilewert zurück."""            
         return self.__beduerfnisse["langeweile"]["wert"]
-    
     @langeweile_punkte_wert.setter
     def langeweile_punkte_wert(self, wert): 
-        self.__beduerfnisse["langeweile"]["wert"] = wert  
-           
+        self.__beduerfnisse["langeweile"]["wert"] = wert           
     @property # Dreck
     def dreck_punkte_wert(self):
         """Gibt den aktuellen Dreckwert zurück."""
         return self.__beduerfnisse["dreck"]["wert"]
-    
     @dreck_punkte_wert.setter
     def dreck_punkte_wert(self, wert): 
         self.__beduerfnisse["dreck"]["wert"] = wert
-        
     @property # Gesundheit
     def gesundheit_punkte_wert(self):
         """Gibt den aktuellen Gesundheitswert zurück."""
         return self.__gesundheit
-    
     @gesundheit_punkte_wert.setter
     def gesundheit_punkte_wert(self, wert): 
         self.__gesundheit = wert
