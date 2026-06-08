e1aa8a0e489041d3d99c8bc6655ad0eb04e050c2
Tim K <klein_tim@t-online.de>
2026-06-08 12:10:22 +0200

    add: Testausgaben hinzugefügt

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pass
     
 if __name__ == "__main__":
-    print(dino.gesundheit_punkte_wert)
-
+    print(f"Hunger: {dino.hunger_punkte_wert}")
+    print(f"Müdigkeit: {dino.muedigkeit_punkte_wert}")
+    print(f"Langeweile: {dino.langeweile_punkte_wert}")
+    print(f"Dreck: {dino.dreck_punkte_wert}")
+    print(f"Gesundheit: {dino.gesundheit_punkte_wert}")
     main()
